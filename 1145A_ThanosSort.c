61c88e4195a3445ad5967f16b0a7f5b1695b1f11
Homie_SMK <46037988+Homie-SMK@users.noreply.github.com>
2020-03-07 22:25:29 +0900

    Update 1145A_ThanosSort.c

--- 1145A_ThanosSort.c
+++ 1145A_ThanosSort.c
@@ -1,3 +1,22 @@
+/*
+  Thanos sort is a supervillain sorting algorithm, 
+  which works as follows: if the array is not sorted, 
+  snap your fingers* to remove the first or the second half of the items, and repeat the process.
+
+  Given an input array, what is the size of the longest sorted array you can obtain from it using Thanos sort?
+
+  *Infinity Gauntlet required.
+
+  <Input>
+  The first line of input contains a single number n (1≤n≤16) — the size of the array. n is guaranteed to be a power of 2.
+
+  The second line of input contains n space-separated integers ai (1≤ai≤100) — the elements of the array.
+
+  <Output>
+  Return the maximal length of a sorted array you can obtain using Thanos sort. 
+  The elements of the array have to be sorted in non-decreasing order.
+*/
+
 #include <stdio.h>
 #include <math.h>
 #define SIZE 20
